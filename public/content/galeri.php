0d4af33d7cc2b6215c565c9326ae8c58c6783700
Mawataribuka <mawataribuka30@gmail.com>
2023-02-15 17:07:44 -0800

    mawa_uas_web2ti3f

---
+++ public/content/galeri.php
@@ -0,0 +1,25 @@
+    <header class="container bg-secondary  text-white">
+        <div class="row">
+            <div class="col-12 py-2 text-center">
+                <h2 class="display-6 ">Galleri </h2>
+            </div>
+        </div>
+    </header>
+<main class="container border p-md-5 p-2">
+        <div class="row g-2 g-md-4">
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="mawa.jpg" alt="Mawa">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="mawa1.jpg" alt="Mawa">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="mawa.jpg" alt="Mawa">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="mawa1.jpg" alt="Mawa">
+            </div>
+            
+        </div>
+    </main>
+
